16f496dafd3e82f113f99b5b560b14e203a59839
Alexis R <alexrafael10@gmail.com>
2022-06-02 12:39:30 -0300

    Home component + added Histoire

---
+++ src/components/pages/TheHome.story.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import TheHome from "./TheHome.vue";
+</script>
+
+<template>
+  <Story>
+    <TheHome></TheHome>
+  </Story>
+</template>
---
+++ src/components/pages/TheHome.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>test</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "TheHome",
+};
+</script>
+
+<script setup lang="ts"></script>
