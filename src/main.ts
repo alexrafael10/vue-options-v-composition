c3c982cebf3f0fcafa5d19b0a9af69758df42022
Alexis R <alexrafael10@gmail.com>
2022-06-16 13:43:31 -0300

    add highlight js and vite plugin static copy

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+
+import "highlight.js/styles/github.css";
 import "./index.css";
+
+import hljs from "highlight.js/lib/core";
+import hljsDefineVue from "highlightjs-vue";
+import hljsVuePlugin from "@highlightjs/vue-plugin";
+import javascript from "highlight.js/lib/languages/javascript";
+import html from "highlight.js/lib/languages/xml";
+
+hljsDefineVue(hljs);
+hljs.registerLanguage("javascript", javascript);
+hljs.registerLanguage("html", html);
 
 const app = createApp(App);
 
 app.use(router);
+app.use(hljsVuePlugin);
 
 app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,21 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+const filesToCopy = [
+  "src/components/simple-sum/SimpleSumComposition.vue",
+  "src/components/simple-sum/SimpleSumOption.vue",
+];
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: filesToCopy.map(src => ({ src, dest: "code-files" })),
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
